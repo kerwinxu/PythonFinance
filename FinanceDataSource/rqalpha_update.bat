252324d2c2d1ddd1e9679a4c011ff6fc1bd27909
kerwinxu <kerwin.cn@gmail.com>
2018-01-13 20:10:33 +0800

    new: user: 增加了一个可以自动更新rqalpha数据源的东西

---
+++ FinanceDataSource/rqalpha_update.bat
@@ -0,0 +1,1 @@
+python rqalpha_update.py
---
+++ FinanceDataSource/rqalpha_update.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Last Change:  2018-01-13 20:04:00
+"""@File Name: rqalpha_update.py
+@Author:  kerwin.cn@gmail.com
+@Created Time:2018-01-13 20:01:44
+@Last Change: 2018-01-13 20:01:44
+@Description : 更新rqalpha的数据的。
+"""
+
+from rqalpha_data import rqalpha_update
+
+rqalpha_update()
