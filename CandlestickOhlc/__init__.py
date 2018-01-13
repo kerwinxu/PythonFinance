d165702f7e81aedaa1cb3f6913e767f72df4a035
kerwinxu <kerwin.cn@gmail.com>
2018-01-13 19:53:22 +0800

    new: user: 将这个做成包

---
+++ CandlestickOhlc/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Last Change:  2018-01-13 15:47:39
+"""@File Name: __init__.py
+@Author:  kerwin.cn@gmail.com
+@Created Time:2018-01-13 15:42:55
+@Last Change: 2018-01-13 15:42:55
+@Description :  包
+"""
+from . import CandlestickOhlc
